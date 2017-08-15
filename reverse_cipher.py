90a504c6a8efa101f3194ca5101a29db48402d30
imze5z <imze5z@aliyun.com>
2017-08-15 22:56:30 +0800

    反转加密法

---
+++ reverse_cipher.py
@@ -0,0 +1,10 @@
+# ReverseCipher
+message = 'Threecankeepasecret,iftwoofthemaredead.'
+translated = ''
+
+i = len(message)-1
+while i >= 0:
+    translated = translated+message[i]
+    i = i - 1
+
+print(translated)
